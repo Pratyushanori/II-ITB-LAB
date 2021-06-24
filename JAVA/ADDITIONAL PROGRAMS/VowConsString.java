9181b99315e01ad9402a94ca2e810b44bcd95032
Pratyushanori <69639988+Pratyushanori@users.noreply.github.com>
2021-06-24 13:55:13 +0530

    Update VowConsString.java

--- JAVA/ADDITIONAL PROGRAMS/VowConsString.java
+++ JAVA/ADDITIONAL PROGRAMS/VowConsString.java
@@ -1,7 +1,7 @@
 public class VowConsString {
 
     public static void main(String[] args) {
-        String str = "EunoiaCryst";
+        String str = "IeMyVisiion";
         int vcount = 0, ccount = 0;
 
         //converting all the chars to lowercase
